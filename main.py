17f5056d79580cc1a78036cf87294258ceaf5605
bobbyhadz <opmftw@gmail.com>
2024-04-09 08:25:20 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Remove the Decimal part from a Float in Python
+
+import math
+
+
+result_1 = math.floor(3.999)
+print(result_1)  # 👉️ 3
+
+result_2 = math.floor(3.14)
+print(result_2)  # 👉️ 3
+
+result_3 = math.floor(-3.6)
+print(result_3)  # 👉️ -4
